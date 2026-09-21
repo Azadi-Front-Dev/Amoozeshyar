c3091ccbed8fc09d761e699c809a5a43d7db78b0
Ali <ali.azadi.dev@gmail.com>
2026-09-21 12:02:12 +0330

    feat: add react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import { useState } from "react";
-
+import { useRoutes } from "react-router-dom";
 import "./App.css";
-function App() {
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import Notfound from "./Pages/Notfound";
 
+const routes = [
+  { path: "/", element: <Home /> },
+  { path: "/Login", element: <Login /> },
+  { path: "*", element: <Notfound /> },
+];
+
+function App() {
+  const router = useRoutes(routes);
   return (
     <>
       <div className=""></div>
+      {router}
     </>
   );
 }
